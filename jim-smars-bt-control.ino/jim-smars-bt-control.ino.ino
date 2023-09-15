5c2de4f8732062649aaaf6507e3b6b9d83b61e2a
Jim Ford <drofmij@gmail.com>
2023-09-15 17:36:57 -0400

    2 modes. controls mostly working as expected. needs finesse.

--- jim-smars-bt-control.ino/jim-smars-bt-control.ino.ino
+++ jim-smars-bt-control.ino/jim-smars-bt-control.ino.ino
@@ -1,93 +1,109 @@
 #include <AFMotor.h>
 #include <Dabble.h>
-// #include <SoftwareSerial.h>
-
-
-// SoftwareSerial btSerial(A4,A5);
 
 
 #define CUSTOM_SETTINGS
 #define INCLUDE_GAMEPAD_MODULE
+#define ROBOT_MODE 0
+#define MANUAL_MODE 1
 
-AF_DCMotor R_motor(3);  // defines Right motor connector
-AF_DCMotor L_motor(4);  // defines Left motor connector
-// declare variables
+// pins for echo sensor
+#define SONIC_TRIGGER_PIN 10
+#define SONIC_ECHO_PIN 9
+int duration;
+int distance;
 
+
+// Motor settings
+// NOTE: Left Motor is backwards.
 #define RM_FORWARD FORWARD
 #define LM_FORWARD BACKWARD
 #define RM_BACKWARD BACKWARD
 #define LM_BACKWARD FORWARD
 
 #define HIGH_SPEED 255
-#define LOW_SPEED 150
+#define LOW_SPEED 100
 
-int distancecm = 0;
-const int trigPin = 10;
-const int echoPin = 9;
-// defines variables
-long duration;
-int distance;
+#define RIGHT_MOTOR_PINS 3
+#define LEFT_MOTOR_PINS 4
+
+AF_DCMotor R_motor(RIGHT_MOTOR_PINS);  // defines Right motor connector
+AF_DCMotor L_motor(LEFT_MOTOR_PINS);  // defines Left motor connector
+
+int controlMode = MANUAL_MODE;
 
 void setup() {
-
-
   Dabble.begin(9600, A4, A5);
   Serial.begin(9600);  // sets up Serial library at 9600 bps
-  // btSerial.begin(9600);
 
   //changes the following values to make the robot  drive as straight as possible
   L_motor.setSpeed(HIGH_SPEED);  // sets L motor speed
   R_motor.setSpeed(HIGH_SPEED);  // sets R motor speed
 
-  //R_motor.run(RELEASE);      //turns L motor on
-  //L_motor.run(RELEASE);      //turns R motor on
-  pinMode(trigPin, OUTPUT);  // Sets the trigPin as an Output
-  pinMode(echoPin, INPUT);   // Sets the echoPin as an Input
+  R_motor.run(RELEASE);      //turns L motor on
+  L_motor.run(RELEASE);      //turns R motor on
+  pinMode(SONIC_TRIGGER_PIN, OUTPUT);  // Sets the trigPin as an Output
+  pinMode(SONIC_ECHO_PIN, INPUT);   // Sets the echoPin as an Input
 }
 
 void loop() {
-  // if (btSerial.available()) {
-  //   Serial.write(btSerial.read());
-  // }
-
-  // if (Serial.available()) {
-  //   btSerial.write(Serial.read());
-  // }
   Dabble.processInput();
-  if (GamePad.isUpPressed()) {
+
+  if (GamePad.isSelectPressed() && controlMode == MANUAL_MODE) {
+    controlMode = ROBOT_MODE;
+    Serial.println("CONTROL MODE: ROBOT");
+    stop();
+    delay(1000);
     forward();
-  }
-  if (GamePad.isDownPressed()) {
-    backward();
-  }
-  if (GamePad.isLeftPressed()) {
-    turnLeft();
-  }
-  if (GamePad.isRightPressed()) {
-    turnRight();
+  } else if(GamePad.isStartPressed() && controlMode == ROBOT_MODE) {
+    stop();
+    delay(1000);
+    controlMode = MANUAL_MODE;
+    Serial.println("CONTROL MODE: Manual");
   }
 
-  if(GamePad.isSquarePressed()) {
+  if (GamePad.isSquarePressed()) {
+    controlMode = MANUAL_MODE;
+    Serial.println("CONTROL MODE: Manual");
     stop();
   }
 
-  // distancecm = checkDistanceCM();
-  // if (distance < 10) {
-  //   stop();
-
-  //   delay(500);
-
-  //   //go backward
-  //   reverse();
-
-  //   // turn
-  //   // todo: randomize turn direction?
-  //   delay(1000);
-  //   turnRight();
-  //   delay(1000);
-  // } else {
-  //   forward();
-  // }
+  if (controlMode == MANUAL_MODE) {
+    if (GamePad.isUpPressed()) {
+      Serial.println("Pressed Forward");
+      forward();
+    }
+    if (GamePad.isDownPressed()) {
+      Serial.println("Pressed Backward");
+      backward();
+    }
+    if (GamePad.isLeftPressed()) {
+      Serial.println("Pressed Left");
+      turnLeft();
+    }
+    if (GamePad.isRightPressed()) {
+      Serial.println("Pressed Right");
+      turnRight();
+    }
+  } else if (controlMode == ROBOT_MODE) {
+    if (checkDistanceCM() < 10) {
+      Serial.println("Found Obstacle");
+      stop();
+      delay(500);
+      //go backward
+      Serial.println("Move Backwards");
+      backward();
+      // turn
+      // todo: randomize turn direction?
+      delay(1000);
+      Serial.println("Turn Right");
+      turnRight();
+      delay(1000);
+      Serial.println("Continue");
+    } else {
+      forward();
+    }
+  }
 }
 
 void forward() {
@@ -98,6 +114,7 @@ void forward() {
 }
 
 void backward() {
+  stop();
   L_motor.setSpeed(LOW_SPEED);  // sets L motor speed
   R_motor.setSpeed(LOW_SPEED);  // sets R motor speed
   R_motor.run(RM_BACKWARD);
@@ -105,6 +122,8 @@ void backward() {
 }
 
 void stop() {
+  R_motor.run(BRAKE);
+  L_motor.run(BRAKE);
   R_motor.run(RELEASE);
   L_motor.run(RELEASE);
 }
@@ -120,18 +139,17 @@ void turnLeft() {
 }
 
 int checkDistanceCM() {
-  digitalWrite(trigPin, LOW);
+  digitalWrite(SONIC_TRIGGER_PIN, LOW);
   delayMicroseconds(2);
-  // Sets the trigPin on HIGH state for 10 micro seconds
-  digitalWrite(trigPin, HIGH);
+  // Sets the SONIC_TRIGGER_PIN on HIGH state for 10 micro seconds
+  digitalWrite(SONIC_TRIGGER_PIN, HIGH);
   delayMicroseconds(10);
-  digitalWrite(trigPin, LOW);
-  // Reads the echoPin, returns the sound wave travel time in microseconds
-  duration = pulseIn(echoPin, HIGH);
+
+  // send ping
+  digitalWrite(SONIC_TRIGGER_PIN, LOW);
+  //receive ping -> returns the sound wave travel time in microseconds
+  duration = pulseIn(SONIC_ECHO_PIN, HIGH);
   // Calculating the distance
   distance = duration * 0.034 / 2;
-  // Prints the distance on the Serial Monitor
-  // Serial.print("Distance: ");
-  // Serial.print(distance);
-  // Serial.print("\n");
+  return distance;
 }
